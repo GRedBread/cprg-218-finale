2fbb6ce120d6af5b5eeecd19297c78ce65633266
GoodRedBread <liam.braun@edu.sait.ca>
2024-12-10 17:29:20 -0700

    a

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-const wrappers = document.querySelectorAll('.image-wrapper');
-wrappers.forEach((wrapper) => {
-  wrapper.addEventListener('mouseenter', () => {
-    wrapper.style.setProperty('--glow', '1');
-  });
-  wrapper.addEventListener('mouseleave', () => {
-    wrapper.style.setProperty('--glow', '0');
-  });
+const albums = document.querySelectorAll('.album');
+
+const fadeInObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.5
 });
+albums.forEach(album => {
+    fadeInObserver.observe(album);
+});
+const albumImages = document.querySelectorAll('.album-image2');
+albumImages.forEach(image => {
+    image.addEventListener('click', (e) => {
+        const audio = e.target.closest('.album').querySelector('audio');
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+});
+const albumImages2 = document.querySelectorAll('.album-image2');
+albumImages2.forEach(image => {
+    image.addEventListener('click', (e) => {
+        const audio = e.target.closest('.card').querySelector('audio');
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+});
+
+
+
